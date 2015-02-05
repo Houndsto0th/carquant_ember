499b3227b3e6e11dea94b0a0142b364357792a93
Houndsto0th <bry.kinnear@gmail.com>
2015-02-05 10:26:36 -0700

    fixed model dropdown issue with bind-attr

--- app/controllers/comparison.js
+++ app/controllers/comparison.js
@@ -4,15 +4,35 @@ export default Ember.Controller.extend({
 
   actions: {
 
+    // carModel should display after a make has been selected, it's value should be reset
+    // if a different make is selected.
+
     didChooseMake: function (make) {
       this.set('make', make);
-      this.set('carModel', null);
+      this.set('carModel', null)
+      this.set('styles', []);
+      this.set('year', null);
     },
 
     didChooseModel: function (model) {
       this.set('carModel', model);
-    }
+    },
 
+    didChooseYear: function(make, model, year){
+      var promise = $.getJSON(
+        'https://api.edmunds.com/api/vehicle/v2/'+ make.name.toString() +
+        '/'+
+        model.name.toString() +
+        '/'+
+        year.year.toString() +
+        '/styles?fmt=json&api_key=aqtqbdzr9ehs27chq6tre3k7&view=full'
+      );
+
+      this.set('year', year);
+      promise.then(function (json) {
+        this.set('styles', json.styles);
+      }.bind(this));
+    }
   }
 
 });
--- app/routes/comparison.js
+++ app/routes/comparison.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return $.getJSON('https://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=aqtqbdzr9ehs27chq6tre3k7');
-  }
+  },
 });
